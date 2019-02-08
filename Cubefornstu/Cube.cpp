686d7f83deec689b78d27996da648645a690b1b6
Alexander Bylaew <superman48720@gmail.com>
2019-02-08 19:00:02 +0700

    release v0.1

---
+++ Cubefornstu/Cube.cpp
@@ -0,0 +1,97 @@
+﻿#include <glut.h>
+
+
+char title[] = "[CG] Bulaev NSTU Lab 1";
+GLfloat angle = 0.0f;     
+int refreshMills = 15;
+
+void initGL(void) {
+	glClearColor(0.047f, 0.047f, 0.047f, 0.0f); 
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);				      
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST); 
+}
+void drawCube(void) {
+	glBegin(GL_QUADS);
+	glColor3f(0.471f, 0.337f, 0.584f);
+	glVertex3f(1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(1.0f, 1.0f, 1.0f);
+	// Bottom face (y = -1.0f)
+	glColor3f(0.01f, 0.66f, 0.956f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	// Front face  (z = 1.0f)
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	// Back face (z = -1.0f)
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, -1.0f);
+
+	// Left face (x = -1.0f)
+	glColor3f(1, 0.23f, 0.0f);
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+
+	// Right face (x = 1.0f)
+	glColor3f(0.96f, 0.96f, 0.96f);
+	glVertex3f(1.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glEnd();
+}
+
+void display(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.0f, 0.0f, -5.0f);
+	glRotatef(angle, 1.0f, 1.0f, 1.0f);
+	drawCube();
+	glutSwapBuffers();
+	angle += 0.2f;
+}
+
+void timer(int value) {
+	glutPostRedisplay();
+	glutTimerFunc(refreshMills, timer, 0);
+}
+
+void reshape(GLsizei width, GLsizei height) { 
+	if (height == 0) {
+		height = 1;
+	}
+	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);         
+	glutInitDisplayMode(GLUT_DOUBLE);
+	glutInitWindowSize(640, 480);
+	glutInitWindowPosition(50, 50);
+	glutCreateWindow(title);
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	initGL();
+	glutTimerFunc(0, timer, 0);
+	glutMainLoop();
+	return 0;
+}

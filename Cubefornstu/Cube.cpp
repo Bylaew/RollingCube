0c17d900d3a1cd3118077fd49456ff9c53860b11
Alexander Bylaew <superman48720@gmail.com>
2019-02-08 19:20:22 +0700

    Update Cube.cpp

--- Cubefornstu/Cube.cpp
+++ Cubefornstu/Cube.cpp
@@ -2,73 +2,70 @@
 
 
 char title[] = "[CG] Bulaev NSTU Lab 1";
-GLfloat angle = 0.0f;     
-int refreshMills = 15;
+GLfloat angleForCubeRotate = 0.0f;     
+int rm = 15;
 
-void initGL(void) {
-	glClearColor(0.047f, 0.047f, 0.047f, 0.0f); 
+void initGL() {
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f); 
 	glClearDepth(1.0f);
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);				      
 	glShadeModel(GL_SMOOTH);
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST); 
 }
-void drawCube(void) {
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(-0.5f, 0.0f, -5.0f);
+	glRotatef(angleForCubeRotate, 1.0f, 1.0f, 1.0f);
 	glBegin(GL_QUADS);
-	glColor3f(0.471f, 0.337f, 0.584f);
+	glColor3f(0.0f, 0.0f, 0.0f);		// Select your own color
 	glVertex3f(1.0f, 1.0f, -1.0f);
 	glVertex3f(-1.0f, 1.0f, -1.0f);
 	glVertex3f(-1.0f, 1.0f, 1.0f);
 	glVertex3f(1.0f, 1.0f, 1.0f);
-	// Bottom face (y = -1.0f)
-	glColor3f(0.01f, 0.66f, 0.956f);
+	// YYYYYYY
+	glColor3f(0.0f, 0.0f, 0.0f);		// Select your own color
 	glVertex3f(1.0f, -1.0f, 1.0f);
 	glVertex3f(-1.0f, -1.0f, 1.0f);
 	glVertex3f(-1.0f, -1.0f, -1.0f);
 	glVertex3f(1.0f, -1.0f, -1.0f);
-	// Front face  (z = 1.0f)
-	glColor3f(1.0f, 0.0f, 0.0f);
+	// ZZZZZZZZ
+	glColor3f(0.0f, 0.0f, 0.0f);		// Select your own color
 	glVertex3f(1.0f, 1.0f, 1.0f);
 	glVertex3f(-1.0f, 1.0f, 1.0f);
 	glVertex3f(-1.0f, -1.0f, 1.0f);
 	glVertex3f(1.0f, -1.0f, 1.0f);
-	// Back face (z = -1.0f)
-	glColor3f(1.0f, 1.0f, 0.0f);
+	// -ZZZZZZZ
+	glColor3f(0.0f, 0.0f, 0.0f);		// Select your own color
 	glVertex3f(1.0f, -1.0f, -1.0f);
 	glVertex3f(-1.0f, -1.0f, -1.0f);
 	glVertex3f(-1.0f, 1.0f, -1.0f);
 	glVertex3f(1.0f, 1.0f, -1.0f);
 
-	// Left face (x = -1.0f)
-	glColor3f(1, 0.23f, 0.0f);
+	// -XXXXXXX
+	glColor3f(0.0f, 0.0f, 0.0f);		// Select your own color
 	glVertex3f(-1.0f, 1.0f, 1.0f);
 	glVertex3f(-1.0f, 1.0f, -1.0f);
 	glVertex3f(-1.0f, -1.0f, -1.0f);
 	glVertex3f(-1.0f, -1.0f, 1.0f);
 
-	// Right face (x = 1.0f)
-	glColor3f(0.96f, 0.96f, 0.96f);
+	// XXXXXXXX
+	glColor3f(0.0f, 0.0f, 0.0f);		// Select your own color
 	glVertex3f(1.0f, 1.0f, -1.0f);
 	glVertex3f(1.0f, 1.0f, 1.0f);
 	glVertex3f(1.0f, -1.0f, 1.0f);
 	glVertex3f(1.0f, -1.0f, -1.0f);
 	glEnd();
-}
-
-void display(void) {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	glTranslatef(0.0f, 0.0f, -5.0f);
-	glRotatef(angle, 1.0f, 1.0f, 1.0f);
-	drawCube();
 	glutSwapBuffers();
-	angle += 0.2f;
+	angleForCubeRotate += 0.2f;
 }
 
 void timer(int value) {
 	glutPostRedisplay();
-	glutTimerFunc(refreshMills, timer, 0);
+	glutTimerFunc(rm, timer, 0);
 }
 
 void reshape(GLsizei width, GLsizei height) { 

d2cb9bd07272b3274f602b6c1a5e4c0e91939fd5
Alexander Bylaew <superman48720@gmail.com>
2019-02-10 00:27:42 +0700

    Update Cube.cpp

--- Cubefornstu/Cube.cpp
+++ Cubefornstu/Cube.cpp
@@ -19,7 +19,7 @@ void display() {
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glTranslatef(-0.5f, 0.0f, -7.0f);
-	glRotatef(angleForCubeRotate, 1.0f, 1.0f, 0.0f);
+	glRotatef(angleForCubeRotate, 1.0f, 1.0f, 1.0f);
 	glBegin(GL_QUADS);
 	glColor3f(0.0f, 0.0f, 0.0f);		// Select your own color
 	glVertex3f(1.0f, 1.0f, -1.0f);
